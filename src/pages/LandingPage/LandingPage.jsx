3016ba41b787d27a3f31adbdbb68339c2e5ad32b
high-haseeb <haseebkhalidoriginal@gmail.com>
2024-07-31 02:30:00 +0500

    added "responsiveness" to the food items lottie

--- src/pages/LandingPage/LandingPage.jsx
+++ src/pages/LandingPage/LandingPage.jsx
@@ -1,10 +1,10 @@
-import React, { useEffect } from "react";
+import { useEffect } from "react";
 import "./LandingPage.css";
 
 import Wrapper from "../../reusableComponents/Wrapper/Wrapper";
 import { useNavigate } from "react-router-dom";
 
-import Lottie from 'react-lottie';
+import Lottie from "react-lottie";
 import animationData from "../../assets/images/lottie_files/heartbeatlogo.json";
 import foodElementsJson from "../../assets/images/lottie_files/new.json";
 import architectureJson from "../../assets/images/lottie_files/architecture-nobg.json";
@@ -22,57 +22,38 @@ const LandingPage = () => {
     autoplay: true,
     animationData: animationData,
     rendererSettings: {
-      preserveAspectRatio: 'xMidYMid slice'
-    }
+      preserveAspectRatio: "xMidYMid slice",
+    },
   };
   const foodElements = {
     loop: false,
     autoplay: true,
     animationData: foodElementsJson,
     rendererSettings: {
-      preserveAspectRatio: 'xMidYMid slice'
-    }
+      preserveAspectRatio: "xMidYMid slice",
+    },
   };
   const architecture = {
     loop: false,
     autoplay: true,
     animationData: architectureJson,
     rendererSettings: {
-      preserveAspectRatio: 'xMidYMid slice'
-    }
+      preserveAspectRatio: "xMidYMid slice",
+    },
   };
   return (
     <Wrapper>
-        {/* <div className="lottie_animation">
-          <Lottie options={defaultOptions} height='100vh' width={500} />
-        </div> */}
       <div className="landing_page_wrapper">
-      <div className="foodElements" >
-
-<Lottie options={foodElements}  width='100%'/>
-</div>
+        <div style={{ width: "100%", height: "auto", position: "absolute", top: "37%", left: "50%", transform: "translateX(-50%) translateY(-50%)" }}>
+          <Lottie options={foodElements} style={{ objectFit: "cover" }} />
+        </div>
         <div className="landing_logo_wrapper ">
-        
-          <Lottie className="" options={defaultOptions} height='300px' width='200px' />
+          <Lottie className="" options={defaultOptions} height="300px" width="200px" />
         </div>
 
-     
-        <div className="landing_footer_wrapper"> 
-        <Lottie className="" options={architecture}  />
-          {/* <img
-            src={FooterPlaces}
-            className="img-fluid footer_place animate__animated animate__rotateInDownLeft"
-            alt="Emojis"
-          />
-          <img
-            src={FooterWave}
-            className="img-fluid footer_wave animate__animated animate__bounceInUp"
-            alt="Emojis"
-          /> */}
-        </div> 
-
-       
-   
+        <div className="landing_footer_wrapper">
+          <Lottie className="" options={architecture} />
+        </div>
       </div>
     </Wrapper>
   );
