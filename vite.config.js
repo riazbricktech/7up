a46e1b581ec4ea22f9a54958cb06e5b63b537edb
riazbricktech <riaz@bricktech.dev>
2024-08-01 15:00:06 +0500

    access server to redirect link

--- vite.config.js
+++ vite.config.js
@@ -18,11 +18,21 @@
 // });
 
 
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
-
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom'],
+        },
+      },
+    },
+  },
+  server: {
+    historyApiFallback: true,
+  },
+});
