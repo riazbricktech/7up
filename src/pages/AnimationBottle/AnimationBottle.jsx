617ff32ffe8f007115765403cfcb634831fe6f62
high-haseeb <haseebkhalidoriginal@gmail.com>
2024-08-03 23:41:51 +0500

    bottle bounce convert gif -> webm

--- src/pages/AnimationBottle/AnimationBottle.jsx
+++ src/pages/AnimationBottle/AnimationBottle.jsx
@@ -1,4 +1,3 @@
-import { useEffect } from "react";
 import "./AnimationBottle.css";
 import Wrapper from "../../reusableComponents/Wrapper/Wrapper";
 import PakImage from "../../assets/images/new_images/pak-logo.webp";
@@ -10,32 +9,12 @@ import { useNavigate } from "react-router-dom";
 const AnimationBottle = () => {
   const navigate = useNavigate();
 
-  useEffect(() => {
-    const handleLoad = () => {
-      const navigate_timer = setTimeout(() => {
-        navigate("/form");
-      }, 6000);
-      return () => {
-        clearTimeout(navigate_timer);
-      };
-    };
-
-    if (document.readyState === "complete") {
-      handleLoad();
-    } else {
-      window.addEventListener("load", handleLoad);
-      return () => {
-        window.removeEventListener("load", handleLoad);
-      };
-    }
-  }, [navigate]);
-
   return (
     <Wrapper>
       <div className="animation_page_wrapper">
         <div className="animation_header_wrapper">
           <div className={`header_mask img-fluid header_lights img-fluid headerMaskExit`}>
-            <Lottie animationData={HeaderLottie} autoPlay={true} loop={false} height="120px"/>
+            <Lottie animationData={HeaderLottie} autoPlay={true} loop={false} height="120px" />
           </div>
         </div>
 
@@ -44,25 +23,15 @@ const AnimationBottle = () => {
             <img src={PakImage} className="img-fluid" alt="Meal Image" />
           </div>
         </div>
-
-        <div
-          style={{
-            width: "100%",
-            height: "auto",
-            zIndex: "0",
-            position: "absolute",
-            top: "0%",
-            left: "50%",
-            transform: "translateX(-50%) translateY(20%)",
-            objectFit: "cover"
-          }}
-        >
-          <Lottie animationData={food} loop={false} />
-        </div>
-
       </div>
-      <div className="animate-bottle">
-        <img src={"/bottle-anim.gif"} className="bottle-animation" alt="Meal Image" />
+
+      <video autoPlay muted playsInline onEnded={() => navigate("/form")} className="z-20">
+        <source src="/output.webm" type="video/webm" />
+        Your browser does not support the video tag.
+      </video>
+
+      <div className="abs z-10 top-10">
+        <Lottie animationData={food} loop={false} />
       </div>
     </Wrapper>
   );
